1b2a6c5683b1f53334901403268592f909ae1f34
Jinny <jinny.choi07@gmail.com>
2024-11-28 15:56:22 -0800

    Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import ImportButton from './components/ImportButton';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <h1>COURT MANAGER</h1>
-        button
+        <ImportButton></ImportButton>
       </header>
     </div>
   );
