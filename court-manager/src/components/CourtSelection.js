82841042a1690539684c866bb1e35c7e9b5acd3d
Jinny <jinny.choi07@gmail.com>
2024-12-15 10:52:32 -0800

    Court component

--- court-manager/src/components/CourtSelection.js
+++ court-manager/src/components/CourtSelection.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Court from './Court';
 
 function CourtSelection({
   numTotCourts,
@@ -33,43 +34,15 @@ function CourtSelection({
       }}
     >
       {Array.from({ length: numTotCourts }, (_, i) => (
-        <div
-          key={i}
-          onClick={() => handleSquareClick(i)} // Click Event
-          style={{
-            width: '150px',
-            height: '150px',
-            backgroundColor: 'lightblue',
-            color: 'black',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            fontSize: '14px',
-            fontWeight: 'bold',
-            border: '1px solid darkblue',
-            borderRadius: '8px',
-            padding: '5px'
-          }}
-        >
-          <div
-            style={{
-              marginBottom: '10px',
-              fontSize: '18px',
-              fontWeight: 'bold'
-            }}
-          >
-            Court {i + 1}
-          </div>
-          <div>
-            {courtAssignments[i + 1] && courtAssignments[i + 1].length > 0 ? (
-              courtAssignments[i + 1].map((name, idx) => (
-                <div key={idx}>{name}</div>
-              ))
-            ) : (
-              <div>No Players</div>
-            )}
-          </div>
+        <div>
+          <Court num={i + 1} isSelected={selectedSquares[i]}></Court>
+          {courtAssignments[i + 1] && courtAssignments[i + 1].length > 0 ? (
+            courtAssignments[i + 1].map((name, idx) => (
+              <div key={idx}>{name}</div>
+            ))
+          ) : (
+            <div>No Players</div>
+          )}
         </div>
       ))}
     </div>
