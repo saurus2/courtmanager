5430f6823e23f01b2a010845fc69fdc44545b522
Jihyeok <bluesaurus2@gmail.com>
2025-01-20 15:42:05 -0800

    Title: adding local storage Content: 1. after import excel, can use refresh without removing data.

--- court-manager/src/App.js
+++ court-manager/src/App.js
@@ -1,12 +1,48 @@
 import './App.css';
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import ImportButton from './components/ImportButton';
 import StatusTable from './components/StatusTable';
 import Assignment from './components/Assignment';
 
 function App() {
-  const [players, setPlayers] = useState([]);
-  const currentStartIndex = useRef(0);
+  // loading data from localStorage when components are mounted
+  const [players, setPlayers] = useState(() => {
+    const savedPlayers = localStorage.getItem('players');
+    return savedPlayers ? JSON.parse(savedPlayers) : [];
+  });
+
+  const [courts, setCourts] = useState(() => {
+    const savedCourts = localStorage.getItem('courts');
+    return savedCourts
+      ? JSON.parse(savedCourts)
+      : Array(8).fill(null).map((_, i) => ({
+          courtIndex: i,
+          isSelected: false,
+          players: []
+        }));
+  });
+
+  // saving updated currentStartIndex on localStorage
+  const currentStartIndex = useRef(
+    localStorage.getItem('currentStartIndex')
+      ? parseInt(localStorage.getItem('currentStartIndex'), 10)
+      : 0
+  );
+
+  // saving data function
+  useEffect(() => {
+    localStorage.setItem('players', JSON.stringify(players)); // saving data on localStorage when players status was updated
+  }, [players]);
+
+  useEffect(() => {
+    localStorage.setItem('courts', JSON.stringify(courts));
+  }, [courts]);
+
+  // saving changed currentStartIndex on localStorage
+  const updateStartIndex = (newIndex) => {
+    currentStartIndex.current = newIndex;
+    localStorage.setItem('currentStartIndex', newIndex.toString()); // saving start index
+  };
 
   return (
     <div className='App p-8'>
@@ -16,6 +52,7 @@ function App() {
           <ImportButton
             shouldShowTestButton={false}
             setPlayers={setPlayers}
+            setCourts={setCourts}
           ></ImportButton>
           <StatusTable
             players={players}
@@ -28,7 +65,10 @@ function App() {
             numTotCourts={8}
             players={players}
             setPlayers={setPlayers}
+            courts={courts}
+            setCourts={setCourts}
             currentStartIndex={currentStartIndex}
+            updateStartIndex={updateStartIndex} // transferring start index
           ></Assignment>
         </div>
       </div>
